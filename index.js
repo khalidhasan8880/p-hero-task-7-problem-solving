f0813ecdb7fac55adeb5ca894124e5113bcf3113
khalidhasan8880 <khalidhasan8880@gmail.com>
2023-07-25 15:45:16 +0600

    task -2 done

--- index.js
+++ index.js
@@ -1,11 +1,28 @@
 // Task 1 : Create a function that takes a string as input and returns the reversed version of the string without using the built-in reverse() method. 
 
-const string = 'hello word';
-let reversedString = '';
-for (let i = string.length -1; i >= 0; i--) {
-    const letter = string[i];
-    reversedString += letter;
+// const string = 'hello word';
+// let reversedString = '';
+// for (let i = string.length -1; i >= 0; i--) {
+//     const letter = string[i];
+//     reversedString += letter;
+// }
+// console.log(reversedString);
+// ------------------------------------
+// ------------------------------------
+
+// Task 2 : Create a function that takes an array of numbers as input and returns the sum of all positive numbers in the array. 
+
+const sumOfPositiveNumbers = (numbers) =>{
+    let sum = 0;
+    for (const number of numbers) {
+        if (number % 2 === 0) {
+            sum += number
+        }
+    }
+    console.log(sum);
+    return sum
 }
-console.log(reversedString);
+
+sumOfPositiveNumbers([1,2,3,4])
 // ------------------------------------
 // ------------------------------------
